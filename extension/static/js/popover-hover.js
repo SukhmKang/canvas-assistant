d5ef7d06af4bd8b647157d2fbf427f3ee3370b7a
SukhmKang <86020052+SukhmKang@users.noreply.github.com>
2022-03-24 10:53:05 -0400

    Add files via upload

---
+++ extension/static/js/popover-hover.js
@@ -0,0 +1,6 @@
+$('[data-toggle="popover-hover"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'bottom',
+    content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+  });
