4ab6057dc0edca9b9bf1d425202a1cc47aadc7b3
SukhmKang <86020052+SukhmKang@users.noreply.github.com>
2022-03-24 10:52:07 -0400

    Delete static directory

--- static/js/popover-hover.js
+++
@@ -1,6 +0,0 @@
-$('[data-toggle="popover-hover"]').popover({
-    html: true,
-    trigger: 'hover',
-    placement: 'bottom',
-    content: function () { return '<img src="' + $(this).data('img') + '" />'; }
-  });
